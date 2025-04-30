dee6fb6079ccf876f17e451c5167e9f1d00035fe
Artur <arturbadona@gmail.com>
2025-04-30 13:44:59 -0300

    update footer add links

--- frontend/src/app/components/footer/index.tsx
+++ frontend/src/app/components/footer/index.tsx
@@ -27,27 +27,26 @@ export function Footer() {
                 <div className={styles.links_sec}>
                     <img id={styles.unimar_logo} src="unimar_logo.svg" alt="unimar_logo" />
                     <h2>Av. Higino Muzi Filho, 1001</h2>
-                    <a href="">+55 (14) 99849-2952</a>
-                    <a href="">falecom@unimar.br</a>
+                    <p>+55 (14) 99849-2952</p>
+                    <p>falecom@unimar.br</p>
                     <div id={styles.links_icons}>
-                        <a href=""><img className={styles.icons} src="youtube.svg" alt="" /></a>
-                        <a href=""><img className={styles.icons} src="facebook.svg" alt="" /></a>
-                        <a href=""><img className={styles.icons} src="whatsapp.svg" alt="" /></a>
+                        <a href="https://www.youtube.com/user/UnimarTV"><img className={styles.icons} src="youtube.svg" alt="" /></a>
+                        <a href="https://www.facebook.com/universidadedemarilia/"><img className={styles.icons} src="facebook.svg" alt="" /></a>
+                        <a href="https://api.whatsapp.com/send?phone=5514998492952"><img className={styles.icons} src="whatsapp.svg" alt="" /></a>
                     </div>
                 </div>
                 <div className={styles.links_sec}>
                     <h3>Unimar</h3>
-                    <a href="">Sobre nós</a>
-                    <a href="">Institucional</a>
-                    <a href="">Universo Unimar</a>
-                    <a href="">Blog</a>
+                    <a href="https://oficial.unimar.br/universidade/">Institucional</a>
+                    <a href="https://oficial.unimar.br/universo-unimar/">Universo Unimar</a>
+                    <a href="https://blog.unimar.br/?_gl=1*13nxmw*_gcl_au*MTI0MzU1MDA4LjE3NDU0MzIyMDA.">Blog</a>
                 </div>
                 <div className={styles.links_sec}>
                     <h3>Ajuda</h3>
-                    <a href="">Consultas e Diplomas</a>
-                    <a href="">Contato</a>
-                    <a href="">Identidade Visual</a>
-                    <a href="">Ouvidoria</a>
+                    <a href="https://oficial.unimar.br/consulta-de-diplomas/">Consultas e Diplomas</a>
+                    <a href="https://oficial.unimar.br/contato/">Contato</a>
+                    <a href="https://oficial.unimar.br/identidade-visual/">Identidade Visual</a>
+                    <a href="https://portal.unimar.br/ouvidoria/?_gl=1*13is9gq*_gcl_au*MTI0MzU1MDA4LjE3NDU0MzIyMDA.">Ouvidoria</a>
                 </div>
             </div>
             <p id={styles.copyright}>Copyright © Unimar 2025</p>
