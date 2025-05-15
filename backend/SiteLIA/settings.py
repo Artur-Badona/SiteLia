df74564fd9a10acfe4fccfe4b78411491abab13f
Artur <arturbadona@gmail.com>
2025-05-15 09:33:44 -0300

    sqlite branch

--- backend/SiteLIA/settings.py
+++ backend/SiteLIA/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'SiteLIA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST', '127.0.0.1'),
-        'PORT': env.str('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
